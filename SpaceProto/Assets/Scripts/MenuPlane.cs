f0fb8b60409abe1f424726f2a6384e3441cecca7
Iain Dowling <ipspdowling@gmail.com>
2015-11-14 15:43:55 +1100

    Updated the menu scene

---
+++ SpaceProto/Assets/Scripts/MenuPlane.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPlane : MonoBehaviour {
+
+    public float maxHeight;
+    public float minHeight;
+
+    void LateUpdate()
+    {
+
+        if (transform.position.y > maxHeight)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, maxHeight, 0), 0.1f);
+        }
+        if (transform.position.y < minHeight)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, minHeight, 0), 0.1f);
+        }
+    }
+}
