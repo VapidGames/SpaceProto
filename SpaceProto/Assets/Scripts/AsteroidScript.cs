1a100354d650f612892096bedbabde335fc3b200
Andrew Giannopoulos <andrew.giannopoulos@gmail.com>
2015-11-14 18:18:09 +1100

    Added asteroids

--- SpaceProto/Assets/Scripts/AsteroidScript.cs
+++ SpaceProto/Assets/Scripts/AsteroidScript.cs
@@ -3,15 +3,13 @@ using System.Collections;
 
 public class AsteroidScript : MonoBehaviour {
 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // kill player
+        }
+    }
 
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
-	}   
 }
