bec20f93197dcfa77ce8e766ad3ef51e7f7ca2cb
Andrew Giannopoulos <andrew.giannopoulos@gmail.com>
2015-11-15 15:10:25 +1100

    Added scrolling nebula background to menu

---
+++ SpaceProto/Assets/NebulaScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NebulaScript : MonoBehaviour {
+
+    Renderer nebulaRenderer;
+
+    public GameObject plane;
+
+	// Use this for initialization
+	void Start () {
+        nebulaRenderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nebulaRenderer.material.mainTextureOffset = new Vector2(plane.transform.position.y * 0.001f - 0.25f, 0);
+	}
+}
